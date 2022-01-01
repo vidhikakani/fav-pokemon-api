fddee7de04b569221f94a41fe14312c3062d6d99
vidhi <vidhikakani1110@gmail.com>
2022-01-01 14:27:44 -0600

    add postgres db url

--- database/database.py
+++ database/database.py
@@ -6,7 +6,7 @@ from  sqlalchemy.orm import sessionmaker
 
 # DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/edvora"
 # DATABASE_URL = "postgresql://njyhztdsfgdunn:d8742da6d677874b3e51d43e254a05aefd44732e3c191c308edb5c57e8e06788@ec2-34-236-87-247.compute-1.amazonaws.com:5432/dd7c4nddvif0ca"
-DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = os.environ.get('POSTGRES_DATABASE_URL')
 engine = create_engine(DATABASE_URL, echo=True)
 
 # Create database if it does not exist.
